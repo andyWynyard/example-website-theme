5399c9e6381a53f8edba286af23a48a3978ae817
andyWynyard <andrew.wynyard@gmail.com>
2020-09-07 10:47:44 +0200

    revert

---
+++ prismic-configuration.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiEndpoint: 'https://test-cli.cdn.prismic.io/api/v2',
+
+  linkResolver: function (doc, ctx) {
+    if (doc.type == 'page') return '/page/' + doc.uid
+    return '/'
+  },
+}
